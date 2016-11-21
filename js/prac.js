1638662e026bbccaeb27358cc2f98d72630bd0f7
Krystian Kobus <kontakt@krystiankobus.pl>
2016-11-21 02:08:43 +0100

    Rebuild script

--- js/prac.js
+++ js/prac.js
@@ -1,31 +1,34 @@
-$('.add-employee-btn').click(function() {
-	var value = $('.employee-name-input').val();
-   	var message = $('#message');
-		
-	if (value == '') { // error if input is blank
-		message.text("error! Pleas input employee name!").css('color','#960606');  
-	} else { // adding new element to list
-		var removeButton = '<button class="remove">X</button>',
-			li = '<li>' + value + removeButton + '</li>';
-	    
-	$('.employee-list').append(li);
-		$('.employee-name-input').val("");
-		message.text("successful! You added new employee to your list!").css('color','green');    
-      }
-	$('.remove').click(function() { // removing element from list
-		var listItem = $(this).parent(), 
-     		deleteBox = $('#delete-box');
-     	deleteBox.css('display','block');
-       		
-       	$('#yes').click(function(){ // user approved to delete element from list
-           	listItem.remove();
-            deleteBox.css('display','');
-           	message.text("User has been succesfully removed!").css('color','');
-        });   
-       	$('#no').click(function(){ // user disapproved to delete element from list
-            deleteBox.css('display','');
-            message.text("");
+function employee() {
+    $('.add-employee').click(function () {
+        var value = $('.employee-name').val();
+        var message = $('#message');
+        if (value == '') {
+            message.text("error! Pleas input employee name!").css('color', '#960606');
+        }
+        else {
+            for (var i = 0; i < $('.employee-list li').length + 1; i++) var removeBtn = '<button class="remove" id="remove' + i + '">X</button>';
+            var li = '<li id="' + i + '">' + value + removeBtn + '</li>';
+            $('.employee-list').append(li);
+            $('.employee-name').val("");
+            message.text("successful! You added new employee to your list!").css('color', 'green');
+        };
+        var remBtn = $('button.remove');
+        $(remBtn).click(function () {
+            var deleteBox = $('#delete-box');
+            var listItem = $(this).parent().attr('id');
+            deleteBox.css('display', 'block');
+            $('#yes').click(function () {
+                deleteBox.css('display', 'none');
+                message.text("User has been succesfully removed!").css('color', '');
+                var usun = $('li#' + listItem);
+                usun.remove();
+            });
+            $('#no').click(function () {
+                deleteBox.css('display', 'none');
+                listItem = null;
+                message.text("");
+            });
         });
-	 });
-});
-
+    });
+};
+window.onload = employee();
